dffa301deae3bca5ac20b1f54f64faa7e316eee9
Deborah Ganin <deborahganin@iMac-de-Deborah.local>
2020-10-27 11:29:58 +0100

    First commit

---
+++ src/Labyrinth.java
@@ -0,0 +1,23 @@
+public class Labyrinth {
+
+    //champs de l'objet Labyrinth
+
+    int nbLigues;
+    int nbColonnes;
+    valCellule;
+    String ROW;
+
+    //méthodes
+
+    // Construir un nouveau objet valCellule de type array2D de cellules
+    public static void constructeur(int nbLigues, int nbColonnes, String ROW) {
+
+    }
+
+    //le cerveau du programme qui va trouver les chemins
+    //sans parametre car le départ et l'arrivé sont tjrs les mêmes
+    public static void pathFinder() {
+
+    }
+
+}
---
+++ src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ */
+
+public class Solution {
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+
+        int M = in.nextInt();
+        int N = in.nextInt();
+
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        for (int i = 0; i < M; i++) {
+            String ROW = in.nextLine();
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...")
+
+
+        System.out.println("answer");
+    }
+}
